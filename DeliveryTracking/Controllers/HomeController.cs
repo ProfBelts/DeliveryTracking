fa7807dfadab46312da1f37804fef7614a6cc63b
brixterPineda <brixter.pineda@elinnovtech.com>
2024-10-11 14:37:45 +0800

    Convert to MVC

---
+++ DeliveryTracking/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DeliveryTracking.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Result()
+        {
+            return View();
+        }
+
+        public IActionResult NotFound()
+        {
+            return View();
+        }
+
+    }
+    
+}
